f3a384d0d2bb5cb5696d7ad20efd47ae84cbc8c1
codebene <dev.rowoon@gmail.com>
2024-03-05 21:44:25 +0900

    feat: 추상 클래스 생성자 작업 완료

--- src/main/java/com/example/calenderapp/event/AbstractEvent.java
+++ src/main/java/com/example/calenderapp/event/AbstractEvent.java
@@ -1,4 +1,32 @@
 package com.example.calenderapp.event;
 
-public abstract class AbstractEvent {
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+public abstract class AbstractEvent implements Event {
+    private final int id;
+    private String title;
+
+    private ZonedDateTime startAt;
+    private ZonedDateTime endAt;
+    private Duration duration; // 시간 차이
+
+    private final ZonedDateTime createdAt;
+    private ZonedDateTime modifiedAt;
+
+    private boolean deletedYn; // 조회 가능 여부 체크
+
+    public AbstractEvent(int id, String title, ZonedDateTime startAt, ZonedDateTime endAt, Duration duration, ZonedDateTime createdAt, ZonedDateTime modifiedAt, boolean deletedYn) {
+        this.id = id;
+        this.title = title;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.duration = Duration.between(startAt, endAt);
+
+        ZonedDateTime now = ZonedDateTime.now();
+        this.createdAt = now;
+        this.modifiedAt = now;
+
+        this.deletedYn = false;
+    }
 }
