765e464d0cdf3382acc4ef2f6fb98ad0972e45e3
codebene <dev.rowoon@gmail.com>
2024-03-05 21:47:45 +0900

    mod: 상속받는 클래스에 맞춰서 생성자 수정

--- src/main/java/com/example/calenderapp/event/Meeting.java
+++ src/main/java/com/example/calenderapp/event/Meeting.java
@@ -4,8 +4,8 @@ import java.time.Duration;
 import java.time.ZonedDateTime;
 
 public class Meeting extends AbstractEvent {
-    public Meeting(int id, String title, ZonedDateTime startAt, ZonedDateTime endAt, Duration duration, ZonedDateTime createdAt, ZonedDateTime modifiedAt, boolean deletedYn) {
-        super(id, title, startAt, endAt, duration, createdAt, modifiedAt, deletedYn);
+    public Meeting(int id, String title, ZonedDateTime startAt, ZonedDateTime endAt) {
+        super(id, title, startAt, endAt);
     }
 
     @Override
