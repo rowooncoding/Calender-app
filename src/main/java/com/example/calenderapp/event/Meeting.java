cae8d15d0d210d5d0d4d0a73f731f3051d0338a6
codebene <dev.rowoon@gmail.com>
2024-03-05 21:50:17 +0900

    feat, mod: Meeting 클래스 필드 추가 및 그에 따른 생성자 수정

--- src/main/java/com/example/calenderapp/event/Meeting.java
+++ src/main/java/com/example/calenderapp/event/Meeting.java
@@ -2,10 +2,19 @@ package com.example.calenderapp.event;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.util.Set;
 
 public class Meeting extends AbstractEvent {
-    public Meeting(int id, String title, ZonedDateTime startAt, ZonedDateTime endAt) {
+    private Set<String> participants; // 사람은 중복되지 않기 때문에 set 자료구조로 설정
+    private String meetingRoom;
+    private String agenda;
+
+    public Meeting(int id, String title, ZonedDateTime startAt, ZonedDateTime endAt, Set<String> participants, String meetingRoom, String agenda) {
         super(id, title, startAt, endAt);
+
+        this.participants = participants;
+        this.meetingRoom = meetingRoom;
+        this.agenda = agenda;
     }
 
     @Override
